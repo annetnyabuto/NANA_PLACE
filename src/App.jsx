4b515b2f19f33bae938fb1787faebffd65786c45
Joseph Mwangi <korimwangi01@gmail.com>
2025-07-24 13:14:13 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 import { AppContext } from './AppContext'
 import Header from './components/Header'
-import Footer from './components/Footer'
+import Footer from './components/footer'
 import HomePage from './components/HomePage'
 import RestaurantPage from './components/RestaurantPage'
 import CartSidebar from './components/CartSidebar'
 import LoginModal from './components/LoginModal'
 import CheckoutPage from './components/CheckoutPage'
-import DashboardPage from './components/Dashboardpage'
+import DashboardPage from './components/dashboardPage'
 function App() {
   const { currentPage, showLoginModal, showCart } = useContext(AppContext)
 
@@ -17,7 +17,7 @@ function App() {
         return <HomePage />
       case 'restaurant':
         return <RestaurantPage />
-      case 'dashboard':
+      case 'dashboard':                             
         return <DashboardPage />
       case 'checkout':
         return <CheckoutPage />
